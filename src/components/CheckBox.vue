f4ea3f2777da6b8d51897ce8970a48acbb584560
Mohammed Taqi <taqealibade@gmail.com>
2024-03-12 02:47:02 +0300

    scaffold my component and install tailwind

---
+++ src/components/CheckBox.vue
@@ -0,0 +1,29 @@
+<template>
+  <label
+    class="m-3 inline-block p-3 w-56 rounded-md cursor-pointer"
+    :class="checked ? 'bg-green-400 hover:bg-green-300' : 'bg-red-400 hover:bg-red-300'"
+  >
+    <input v-model="checked" class="m-3" type="checkbox" :name="name" />{{ text }}
+  </label>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  value: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const checked = ref(props.value)
+</script>
